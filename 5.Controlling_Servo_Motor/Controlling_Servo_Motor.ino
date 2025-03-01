388e235b816142d96f4974f0fefaca78968381fd
Jitendra Sharma <jitendrasharma7409@gmail.com>
2025-03-02 00:48:49 +0530

    Add files via upload

---
+++ 5.Controlling_Servo_Motor/Controlling_Servo_Motor.ino
@@ -0,0 +1,15 @@
+#include <Servo.h>
+Servo myServo;
+
+void setup() {
+  myServo.attach(9);  // Servo connected to pin 9
+}
+
+void loop() {
+  myServo.write(0);   // Move to 0°
+  delay(1000);
+  myServo.write(90);  // Move to 90°
+  delay(1000);
+  myServo.write(180); // Move to 180°
+  delay(1000);
+}
